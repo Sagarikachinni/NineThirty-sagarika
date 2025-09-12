4c32b222928aa4d032afe79db0b57d9fb14f6b3a
SAGARIKA ELUGOTI <153708282+Sagarikachinni@users.noreply.github.com>
2025-09-12 15:42:20 +0530

    Add files via upload

---
+++ datatypefloat.py
@@ -0,0 +1,11 @@
+#datatype float
+'''s=23.55
+a=12.23
+g=s-a
+print(g)
+print(type(s))
+print(type(a))'''
+
+a=(1,2,2,3,65,7,'saagu',45)
+print(a)
+print(a[5])
---
+++ duplicateRemoving.py
@@ -0,0 +1,4 @@
+#removing duplicates
+list=[2,6,4,7,2,4]
+list.remove(2)
+print(list)
